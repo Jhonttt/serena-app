64068a347e2a93689d75cd545d2cbea15e5612e7
Jhonttt <juanatahonadev@gmail.com>
2026-01-18 23:38:56 +0100

    feat: aplicar protección por roles en rutas

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,49 @@ import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './routes/PrivateRoute';
 
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Públicas */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
+        {/* Ruta privada normal */}
         <Route
           path="/dashboard"
           element={
             <PrivateRoute>
               <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Rutas por rol */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute allowedRoles={['admin']}>
+              <h1>Admin Dashboard</h1>
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/psychologist"
+          element={
+            <PrivateRoute allowedRoles={['psychologist']}>
+              <h1>Psychologist Dashboard</h1>
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/student"
+          element={
+            <PrivateRoute allowedRoles={['student']}>
+              <h1>Student Dashboard</h1>
             </PrivateRoute>
           }
         />
